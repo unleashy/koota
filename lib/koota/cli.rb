a6bea7053ead939fde38ad6eb8489cde2c68eeeb
unleashy <unleashy@users.noreply.github.com>
2019-10-06 17:46:46 -0300

    Implement (most of) the CLI

---
+++ lib/koota/cli.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'koota'
+require 'koota/file_parser'
+
+require 'slop'
+
+module Slop
+  class RangeOption < Option
+    def call(value)
+      split = value.split(',')
+
+      raise Slop::Error, 'invalid range argument' if split.length.zero? || split.length > 2
+
+      split.map!(&:to_i)
+      split.length == 1 ? split[0] : split[0]..split[1]
+    end
+  end
+end
+
+module Koota
+  class CLI
+    def initialize(program_name: $PROGRAM_NAME, output: $stdout)
+      @file_parser = Koota::FileParser.new
+      @generator   = Koota::Generator.new
+      @output      = output
+      @opts        = build_opts(program_name)
+    end
+
+    def call(argv = ARGV)
+      result = @opts.parse(argv)
+
+      if result.help?
+        @output.puts @opts
+        return true
+      elsif result.version?
+        @output.puts "koota v#{VERSION}"
+        return true
+      end
+
+      raise Slop::Error, 'missing input file(s)' if result.args.empty?
+
+      process(result.args, result.to_h)
+    rescue Slop::Error => e
+      @output.puts "error: #{e.message}"
+      @output.puts @opts
+
+      false
+    end
+
+    private
+
+    def process(files, options)
+      is_one_file = files.length == 1
+
+      return false unless verify_existence(files)
+
+      files.each_with_index do |file, i|
+        contents = File.read(file, mode: 'rb', encoding: 'UTF-8')
+        pattern  = @file_parser.call(contents)
+
+        @output.puts "--- #{file}" unless is_one_file
+        @output.puts @generator.call(pattern, options).join(options[:word_separator])
+        @output.puts unless is_one_file || i == files.length - 1
+      end
+
+      true
+    end
+
+    def verify_existence(files)
+      result = true
+
+      files.each do |file|
+        unless File.exist?(file)
+          @output.puts "error: could not find file '#{file}'"
+          result = false
+        end
+      end
+
+      result
+    end
+
+    def build_opts(program_name)
+      banner = <<~BANNER.strip
+        koota v#{VERSION} -- a word generator
+
+        usage: #{program_name} [options] FILE ...
+      BANNER
+
+      Slop::Options.new(banner: banner) do |o|
+        o.bool '-d',
+              '--duplicates',
+              'whether or not to keep duplicate words (default: false)',
+              default: false
+
+        o.range '-s',
+                '--syllables',
+                'the amount of syllables to generate (default: 1)',
+                default: 1
+
+        o.string '-r',
+                '--syllable-separator',
+                'the string separating each syllable (default: empty)',
+                default: ''
+
+        o.int '-w',
+              '--words',
+              'the amount of words to generate (default: 100)',
+              default: 100
+
+        o.string '-p',
+                '--word-separator',
+                'the string separating each word (default: new line)',
+                default: "\n"
+
+        o.separator "\nother options:"
+
+        o.bool '--bytecode', 'output compiled bytecode'
+        o.bool '-h', '--help', 'show help text'
+        o.bool '-v', '--version', 'show version'
+      end
+    end
+  end
+end
---
+++ spec/koota/cli_spec.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+require 'pp' # fixes fakefs weirdness
+require 'fakefs/spec_helpers'
+require 'stringio'
+
+require 'koota/cli'
+
+RSpec.describe Koota::CLI do
+  include FakeFS::SpecHelpers
+
+  USAGE_MSG = 'usage: koota [options] FILE ...'
+
+  let(:output) { StringIO.new(''.dup) }
+  let(:cli) { described_class.new(program_name: 'koota', output: output) }
+
+  describe '#call' do
+    context 'given -h or --help' do
+      it 'shows help', :aggregate_failures do
+        expect(cli.call(['-h'])).to eq(true)
+        expect(output.string).to include(USAGE_MSG)
+
+        output.string = ''.dup
+        expect(cli.call(['--help'])).to eq(true)
+        expect(output.string).to include(USAGE_MSG)
+      end
+
+      it 'overrides all other options' do
+        expect(cli.call(['-v', '-h'])).to eq(true)
+        expect(output.string).to include(USAGE_MSG)
+      end
+    end
+
+    context 'given -v or --version' do
+      it 'shows the version' do
+        expect(cli.call(['-v'])).to eq(true)
+        expect(output.string).to include("koota v#{Koota::VERSION}")
+      end
+    end
+
+    context 'given nothing' do
+      it 'complains' do
+        expect(cli.call([])).to eq(false)
+        expect(output.string).to include('error: missing input file(s)')
+      end
+
+      it 'shows help' do
+        expect(cli.call([])).to eq(false)
+        expect(output.string).to include(USAGE_MSG)
+      end
+    end
+
+    context 'given one file' do
+      context 'and it exists' do
+        it 'processes it' do
+          File.write('test.koota', 'abc')
+          expect(cli.call(['test.koota'])).to eq(true)
+          expect(output.string).to eq("abc\n")
+        end
+      end
+    end
+
+    context 'given files' do
+      context 'and every one exists' do
+        it 'processes each in turn' do
+          File.write('test1.koota', 'abc')
+          File.write('test2.koota', 'def')
+          File.write('test3.koota', 'ghi')
+
+          expect(cli.call(['test1.koota', 'test2.koota', 'test3.koota'])).to eq(true)
+          expect(output.string).to eq(<<~OUTPUT)
+            --- test1.koota
+            abc
+
+            --- test2.koota
+            def
+
+            --- test3.koota
+            ghi
+          OUTPUT
+        end
+      end
+
+      context 'and a file does not exist' do
+        let(:result) do
+          File.write('test1.koota', 'abc')
+          cli.call(['test1.koota', 'test2.koota', 'test3.koota'])
+        end
+
+        it 'complains about non existent files' do
+          expect(result).to eq(false)
+          expect(output.string).to include("error: could not find file 'test2.koota'")
+                               .and include("error: could not find file 'test3.koota'")
+        end
+      end
+
+      it 'takes all the cool options' do
+        File.write('test1.koota', 'a/b')
+        result = cli.call(%w[-d -s 1,2 -w 10 -r . -p ; test1.koota])
+
+        expect(result).to eq(true)
+
+        # match like 'a;a.b;b;b.a;b.b;a;b.a;b;a;b.b'
+        expect(output.string).to match(/\A(?:[ab](?:\.[ab])?;){9}(?:[ab](?:\.[ab])?)\n\z/)
+      end
+    end
+  end
+end
